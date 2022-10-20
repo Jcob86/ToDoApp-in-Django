8155fc2dcd159f9b6ec92e108ab9da04e3572934
Jakub Żmudziński <kubaz8645@gmail.com>
2022-10-20 17:57:14 +0200

    Implemented Full CRUD functionality with basic templates

---
+++ ToDo/app/views.py
@@ -0,0 +1,35 @@
+from plistlib import UID
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Task
+
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'app/task.html'
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
